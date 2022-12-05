db53fd719cfe7124aa94064b68fd0569c342214d
nub69 <shabik.ashab@gmail.com>
2022-12-05 12:09:55 +0600

    train model for get numbers

--- digit_detecting.py
+++ digit_detecting.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 
 digits = load_digits()
@@ -14,4 +15,9 @@ y = digits.target
 # split data for training and testing
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-print(x_train.shape)
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+print('target value', digits.target[1700])
+result = model.predict([digits.data[1700]])
+print('test res', result)
