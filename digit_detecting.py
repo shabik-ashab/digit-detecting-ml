a81823513aba2fb217f17878344bd74644f1a6a2
nub69 <shabik.ashab@gmail.com>
2022-12-05 11:58:57 +0600

    split data for training and testing

--- digit_detecting.py
+++ digit_detecting.py
@@ -1,4 +1,17 @@
 from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 digits = load_digits()
-print(digits)
+
+# plt.gray()
+# plt.matshow(digits.images[0])
+# plt.show()
+
+x = digits.data
+y = digits.target
+
+# split data for training and testing
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+print(x_train.shape)
